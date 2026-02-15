b6fcbc8c3166852931e5878d460ae6d56b9eb518
ciro mc <156836104+Ciricundio@users.noreply.github.com>
2026-02-15 17:04:48 -0500

    Agregamos y mostramos la información del tiquete, incluyendo el número de vuelo, la ciudad de salida, la ciudad de llegada, la hora de salida y la duración del vuelo.

--- ejercicio1/agencia.java
+++ ejercicio1/agencia.java
@@ -29,15 +29,15 @@ public class agencia {
         Ciudad ciudad2 = new Ciudad(2, "Barranquilla");
 
         // Crear un objeto de la clase vuelos
-        vuelos vuelo1 = new vuelos(123, ciudad1.getNombre(), ciudad2.getNombre(), 62, 120);
+        vuelos vuelo1 = new vuelos(123, ciudad1, ciudad2, 62, 120);
 
         //Crear un objeto de la clase pasajero
         pasajero pasajero1 = new pasajero(1, "Ciro Muñoz", 33, "Masculino");
         pasajero pasajero2 = new pasajero(2, "Giovanni Luna", 28, "Femenino");
         pasajero pasajero3 = new pasajero(3, "Natalia Osorio", 4, "Masculino");
 
-        tiquete tiquete1 = new tiquete(1, 20240601, 12);
+        tiquete tiquete1 = new tiquete(1, 20240601, vuelo1, pasajero1, 23);
 
-        JOptionPane.showMessageDialog(null, "Ciudad destino: " + ciudad1.getNombre() + "\nCiudad origen: " + ciudad2.getNombre());
+        tiquete1.mostrarInfo();
     }
 }
